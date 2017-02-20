ff36c757e22a05a3696726e757a88f634b578822
Sangwon Hong <qpakzk@gmail.com>
2017-02-20 15:30:54 +0900

    chapter8

---
+++ chap08/calc_tester2.py
@@ -0,0 +1,5 @@
+from calculator import plus
+from calculator import minus
+
+print(plus(10, 5))
+print(minus(10, 5))
---
+++ chap08/calc_tester3.py
@@ -0,0 +1,4 @@
+from calculator import plus, minus
+
+print(plus(10, 5))
+print(minus(10, 5))
